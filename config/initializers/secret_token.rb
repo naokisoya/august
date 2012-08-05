e4c3dfdea9ecafd201d6e290d8bb531b03290ae2
naokisoya <ringojam12021@gmail.com>
2012-08-05 15:34:25 +0900

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+August::Application.config.secret_token = '8ede6e17d9199d494e028e59eb9750a6c5a4dc6bafa3ed9e1aad75904a31be002c7b87892d0dc61a932d965597f9929dc9daa0c6a9bf497272c35525ef2523fc'
