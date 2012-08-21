b080d2ea78f964b86b66394cf12d687fd4234346
naokisoya <ringojam12021@gmail.com>
2012-08-21 11:02:44 +0900

    ror0910finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.order("created_at DESC")
     @user = User.find(params[:id])
+    @tweets = @user.tweets
   end
 
   def show
