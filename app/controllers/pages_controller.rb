a0a5e6c3f6b4e6ecad6a845a1ce50ba8e1caa9de
naokisoya <ringojam12021@gmail.com>
2012-08-24 11:28:00 +0900

    timeline fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   def home
     if user_signed_in?
-      @tweets = current_user.tweets
+      #@tweets = current_user.tweets
+      @tweets = Tweet.where("user_id = ? OR user_id IN (?)", current_user, current_tuser.friend_ids)
       @title = "Timeline"
       @tweet = Tweet.new
     else
